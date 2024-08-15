075bbd34719697d420d3e7415545e669ea3822a7
SRIJA MAZUMDER <144462715+Srija-Mazumder@users.noreply.github.com>
2024-08-15 21:39:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Box } from '@mui/material';
 import NavBar from './components/NavBar';
- // Assuming you have a Home component
+import CombinedDrawer from './components/SidePanel_stud';
+import Footer from './components/Footer';
 
 function App() {
   return (
-<NavBar/>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <NavBar />
+
+      <Box sx={{ display: 'flex', flexGrow: 1 }}>
+        <CombinedDrawer />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+       
+        </Box>
+      </Box>
+
+      <Footer />
+    </Box>
   );
 }
 
